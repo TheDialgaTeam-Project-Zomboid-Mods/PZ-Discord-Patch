8e63c36627b9ec4983fad70f5eff028e8c0a86d5
Yong Jian Ming <jianming1993@gmail.com>
2024-06-19 20:18:23 +0800

    Better build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,22 +17,11 @@ repositories {
 
 dependencies {
     compileOnly(files("$rootDir/java/"))
-    compileOnly(files("$rootDir/java/commons-compress-1.18.jar"))
-    compileOnly(files("$rootDir/java/istack-commons-runtime.jar"))
-    compileOnly(files("$rootDir/java/jassimp.jar"))
-    compileOnly(files("$rootDir/java/javacord-2.0.17-shaded.jar"))
-    compileOnly(files("$rootDir/java/javax.activation-api.jar"))
-    compileOnly(files("$rootDir/java/jaxb-api.jar"))
-    compileOnly(files("$rootDir/java/jaxb-runtime.jar"))
-    compileOnly(files("$rootDir/java/lwjgl.jar"))
-    compileOnly(files("$rootDir/java/lwjgl_util.jar"))
-    compileOnly(files("$rootDir/java/lwjgl-glfw.jar"))
-    compileOnly(files("$rootDir/java/lwjgl-jemalloc.jar"))
-    compileOnly(files("$rootDir/java/lwjgl-opengl.jar"))
-    compileOnly(files("$rootDir/java/serialize.jar"))
-    compileOnly(files("$rootDir/java/sqlite-jdbc-3.27.2.1.jar"))
-    compileOnly(files("$rootDir/java/trove-3.0.3.jar"))
-    compileOnly(files("$rootDir/java/uncommons-maths-1.2.3.jar"))
+
+    compileOnly(fileTree("$rootDir/java/") {
+        include("*.jar")
+        exclude(rootProject.name + "-*.jar")
+    })
 
     implementation("org.javacord:javacord:3.8.0")
 }
